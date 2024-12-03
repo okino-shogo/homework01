7fe192d24d6d565f770988223932242435ec647e
okino-shogo <okisho0106@gmail.com>
2024-12-03 21:09:40 +0900

    add 1

--- a-7.py
+++ a-7.py
@@ -1,5 +1,6 @@
 even_numbers = [2, 4, 6, 8]
 
 for double_numbers in even_numbers:
-    double_numbers = double_numbers*2
+    double_numbers = double_numbers * 2
     print(double_numbers)
+print(1)
