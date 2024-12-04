40cdafe2f856c8b018f657f75f60bea73be23f71
okino-shogo <okisho0106@gmail.com>
2024-12-04 16:25:21 +0900

    a-8_3domeno-shojiki

--- a-8.py
+++ a-8.py
@@ -1,4 +1,5 @@
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
 for user_date in users_info:
-    print("Name:", user_date[0], ",", "Age:", user_date[1])
+    s = user_date[0]
+    print(f"Name:{s}, Age:", user_date[1])
